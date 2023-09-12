b710a09b1da94fcca975cab5a1d9352d6b0fcff6
codeTerritories <113830981+codeTerritories@users.noreply.github.com>
2023-09-12 12:36:33 +0530

    Update dbConnection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,6 +1,6 @@
 const Mongoose =require('mongoose');
 
-Mongoose.connect(mongodb://127.0.0.1:27017/Database, { useNewUrlParser: true, useUnifiedTopology: true })
+Mongoose.connect("mongodb://127.0.0.1:27017/Database", { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log('Database Connection Success !!');
 })
